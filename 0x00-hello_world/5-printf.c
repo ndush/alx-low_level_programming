dcbb19984c2140172515737264cf89fc5167e22d
ndush <mdamarisnduku@gmail.com>
2022-03-13 19:58:47 +0000

    5th commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  *main -Entry point
-*my hello, printf task
-*Return: return 1 after program execution
-*/
+ *My hello, printf task
+ *Return: return 1 after program execution
+ */
 int main(void)
 {
-printf (with proper grammar, but the outcome is a piece of art.\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-ls
