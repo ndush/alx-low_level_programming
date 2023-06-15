80f0edc5dad1440842d448a7853c1a560d658abc
ndush <mdamarisnduku@gmail.com>
2023-06-15 11:58:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/** main - runs all the code*/
+/** main - runs all the code
+ * Description: run puts and print statement
+ * /
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
