d24bb22019db600072cae18d29352d2e2c97a829
ndush <mdamarisnduku@gmail.com>
2023-06-16 09:25:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,11 +10,10 @@
 int main(void)
 {
 	int l;
-	
        l=
-	sizeof("and that piece of art is useful - Dora Korpa, 2015-10-19");
+	sizeof("and that piece of art is useful" - Dora Korpar, 2015-10-19");
 
 	write(2,
 	"and that piece of art is useful - Dora Korpa, 2015-10-19\n", l);
-	return(1)
+	return(1);
 }
