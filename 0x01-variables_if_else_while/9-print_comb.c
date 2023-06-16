949b45507bf7725dce24a6ce949fc3a0e491f81f
ndush <mdamarisnduku@gmail.com>
2023-06-16 07:43:18 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,11 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
+		putchar(num + '0');
+		if (num != 9){
 		putchar(',');
 		putchar(' ');
+		}
 	}
 
 	putchar('\n');
