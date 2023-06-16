b5d526c8a74cb4dc83fd0f04951658f495c77f2e
ndush <mdamarisnduku@gmail.com>
2023-06-16 10:53:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
-#include<unistd.h>
+#include <unistd.h>
 
 /**
- *main - run all functions
- *
- *Description: Change standard error message
- *Return:return 1 and exit
+ *main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *Return: Always 1
  */
 
 int main(void)
 {
-
-	write(2,"and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", l);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+	return (1)
 }
