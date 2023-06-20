1110b0a0f8603c57a2ff0ca468a3feec172d7acb
ndush <mdamarisnduku@gmail.com>
2023-06-20 14:14:31 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *jack_bauer -  prints every minute of the day of Jack Bauer, 
- *starting from 00:00 to 23:59
+ *jack_bauer -  prints every minute of the day
  *
  * Return: void
  */
