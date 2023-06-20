a696bc9d3995409c3c88a69d9efd7f9030c165bf
ndush <mdamarisnduku@gmail.com>
2023-06-20 13:28:05 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 8-main.c 8-24_hours.c -o 8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *jack_bauer - prints every minute of the day of Jack Bauer, 
+ *starting from 00:00 to 23:59
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int w,x,y,z;
+
+	for (w = 0; w <= 2; w++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if ((w <= 1 && x <= 9) || (w <=2 && x <= 3))
+			{
+				for (y = 0; y <=5; y++)
+				{
+					for (z = 0; z <=9; z++)
+					{
+						_putchar(w + '0');
+						_putchar(x + '0');
+						_putchar(58);
+						_putchar(y + '0');
+						_putchar(z + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+
+	}
+}
