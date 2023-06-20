0f493215fa1c80fed390b9112f55be2c47c1680f
ndush <mdamarisnduku@gmail.com>
2023-06-20 04:50:49 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
+ * description: 'prints _putchar to stdout'
  * reurn: Always 0
  */
 
 int main(void) 
 {
-	putchar('\n);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return 0;
 }
