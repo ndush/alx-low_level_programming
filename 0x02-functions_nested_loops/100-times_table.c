8dc81d9d7ee6e4007bfd15e0654fe3223d50f85e
ndush <mdamarisnduku@gmail.com>
2023-06-21 14:09:15 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,6 @@
  *
  * Return: Nothing
  */
-
 void print_times_table(int n)
 {
 	int i, j, k;
@@ -22,24 +21,21 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				}
-				else if (k < 10 && j !=0)
+				}else if (k < 10 && j !=0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				}
-				else if (k < 10 && k < 100)
+				}else if (k >= 10 && k < 100)
                                 {
                                         _putchar(',');
                                         _putchar(' ');
                                         _putchar(' ');
 					_putchar((k / 10) + '0');
 				       	_putchar((k % 10) + '0');
-                                }
-				else if (k >= 100)
+                                }else if (k >= 100)
                                 {
                                         _putchar(',');
                                         _putchar(' ');
