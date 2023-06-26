59bfadf107e53bcffb9af2c640d3e5e00922a804
ndush <mdamarisnduku@gmail.com>
2023-06-26 15:18:08 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-puts2.c -o 6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *puts2 - prints every other character of a string,
+ *starting with the first character,
+ *@str: input
+ *Return: print
+ */
+void puts2(char *str)
+{
+	int length = 0;
+	int t = 0;
+	char *y = str;
+	int i;
+
+	while (*y != '\0')
+	{
+		y++
+		length++
+	}
+	t = length - 1;
+	for (i = 0; i <= t; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n')
+}
