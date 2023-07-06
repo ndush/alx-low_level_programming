bed4d669703b453b7a18352eccf11b030f743e6f
ndush <mdamarisnduku@gmail.com>
2023-07-05 18:02:02 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,27 +1,29 @@
 #include "main.h"
 
-//int check_pal(char *s, int len);
-//int _strlen_recursion(char *s);
+int check_pal(char *s, int len);
+int _strlen_recursion(char *s);
 /**
- * int is_palindrome -function that returns 1 if a string is a palindrome and 0 if not
+ * is_palindrome -function that returns 1
+ * if a string is a palindrome and 0 if not
  * @s: the string
  * Return: 1 if a string is a palindrome, 0 otherwise
  */
-int is_palindrome(char *s) 
+int is_palindrome(char *s)
 {
-    if(*s == 0)
-        return (1);
-    return (check_pal(s, 0, _strlen_recursion(s)));
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
  * _strlen_recursion - returns the length of a string
  * @s: the string
  * Return: length of the string
 */
-int _strlen_recursion(char *s) 
+int _strlen_recursion(char *s)
 {
-    if (*s == '\0');
-    return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 /**
  * check_pal - checks the characters recursively for palindrome
@@ -29,13 +31,12 @@ int _strlen_recursion(char *s)
  * @i: iterates
  * @len: the length of the string
  * Return: 1 if palindrome is found, 0 otherwise
- * 
-*/
+ */
 int check_pal(char *s, int i, int len)
 {
-    if (*(s + i) != *(s + len - 1))
-        return (0);
-    if (i >= len)
-        return (1);
-    return (check_pal(s, i+ 1, len - 1));
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
