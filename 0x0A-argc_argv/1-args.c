508bcf1723962919381064b3a9cff01bf459117f
ndush <mdamarisnduku@gmail.com>
2023-07-07 14:08:12 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.>
+/**
+ * main - a function that prints the total count of arguments passed
+ *@argc:argument count
+ *@argv:arguments string
+ *Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
