0334ae41ebd0047caad59778031068396b029c91
ndush <mdamarisnduku@gmail.com>
2023-07-17 09:32:00 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - dog's info
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ *Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
