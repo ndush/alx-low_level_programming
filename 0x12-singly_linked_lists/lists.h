8b6aaca2eb4835d9bb225b84d0417f38181a8267
ndush <mdamarisnduku@gmail.com>
2023-07-27 15:18:17 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LINKED
+#define LINKED
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *struct list_s - singly linked list
+ *@str: string
+ *@len: len of the string
+ *@next: points to the nextnode
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void fre_list(list_t *head);
+#endif
